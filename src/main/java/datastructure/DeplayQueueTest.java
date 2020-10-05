803441df34d53b9116605b68a500425c5a9f3263
Lucas-luwh <lwh1561660826@gmail.com>
2020-10-05 20:48:08 +0800

    阻塞队列

---
+++ src/main/java/datastructure/DeplayQueueTest.java
@@ -0,0 +1,17 @@
+package datastructure;
+
+/**
+ * DelayQueue 阻塞队列
+ * 原理：
+ * 1.头部是延迟期满后保存时间最长的 Delayed 元素
+ * 2.在延迟期满时才能从中提取元素
+ * 3.优先队列的比较基准值是时间
+ *
+ * @author julin
+ */
+public class DeplayQueueTest {
+
+	public static void main(String[] args) {
+
+	}
+}
